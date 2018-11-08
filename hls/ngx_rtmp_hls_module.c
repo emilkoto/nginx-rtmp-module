a12d964413320c0ec19a0f940989412614c60f9e
Emil <>
2018-11-08 16:40:04 +0100

    to n

--- hls/ngx_rtmp_hls_module.c
+++ hls/ngx_rtmp_hls_module.c
@@ -1000,7 +1000,7 @@ ngx_rtmp_hls_open_fragment(ngx_rtmp_session_t *s, uint64_t ts,
             strcpy(md5HexResult, hexString(md5hash, MD5_DIGEST_LENGTH, hexBuffer));
             snprintf(md5HexResult, sizeof(md5HexResult), "%.16s", md5HexResult);
             ngx_log_error(NGX_LOG_ERR, s->connection->log, ngx_errno, "MD5 LOG: '%s'", md5HexResult);
-            strcpy(ctx->key, (char *)md5HexResult);
+            strncpy(ctx->key, (unsigned char *)md5HexResult, 16);
 
             ngx_sprintf(ctx->keyfile.data + ctx->keyfile.len, "%uL.key%Z", id);
 
